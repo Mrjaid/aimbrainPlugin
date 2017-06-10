3b2b211b66d31776d6fe3f14ba1cdcc4c73ebcaa
Obinna Maduemesi <jmaduemesi@fasylgh.com>
2017-06-10 15:55:54 +0100

    build bug fix

--- www/aimbrainPlugin.js
+++ www/aimbrainPlugin.js
@@ -3,16 +3,11 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = 'aimbrainPlugin';
 
 var aimbrainPlugin = {
- enrol: function(arg0, success, error) {
-    exec(success, error, PLUGIN_NAME, "enrol", [arg0]);
+    enrol: function (arg0, success, error) {
+        exec(success, error, PLUGIN_NAME, "enrol", [arg0]);
+    },
+    authenticate: function (arg0, success, error) {
+        exec(success, error, PLUGIN_NAME, "authenticate", [arg0]);
+    }
 };
-
-authenticate: function(arg0, success, error) {
-    exec(success, error, PLUGIN_NAME, "authenticate", [arg0]);
-};
-
-  
-};
-
-alert('made it here...');
 window.aimbrainPlugin = aimbrainPlugin;
