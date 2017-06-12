4902ed907a0958510ddff2da5e3318451d1b9aac
Obinna Maduemesi <jmaduemesi@fasylgh.com>
2017-06-12 11:02:00 +0100

    bug fix ...

--- www/aimbrainPlugin.js
+++ www/aimbrainPlugin.js
@@ -1,17 +1,12 @@
 
 
-cordova.define("com.fasyl.aimbrainplugin.aimbrainPlugin", function(require, exports, module) {
-var PLUGIN_NAME = 'aimbrainPlugin';
-
-function aimbrainPlugin() {
-	this.enrol = function (arg0, success, error) {
-     cordova.exec(success, error, PLUGIN_NAME, "enrol", [arg0]);
+cordova.define("com.fasyl.aimbrainplugin.aimbrainPlugin", function (require, exports, module) {
+    module.exports = {
+        enrol: function (arg0, success, error) {
+            cordova.exec(success, error, 'aimbrainPlugin', "enrol", [arg0]);
+        },
+        authenticate: function (arg0, success, error) {
+            cordova.exec(success, error, 'aimbrainPlugin', "authenticate", [arg0]);
+        }
     };
-  this.authenticate = function (arg0, success, error) {
-     cordova.exec(success, error, PLUGIN_NAME, "authenticate", [arg0]);
-   };
-}
-
-window.aimbrainPlugin = new aimbrainPlugin();
-
 });
