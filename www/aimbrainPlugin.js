ca0af121187fe1a2f5d742b01bedd91d09265ec3
Obinna Maduemesi <jmaduemesi@fasylgh.com>
2017-06-13 15:47:14 +0100

    resolve android manifest

--- www/aimbrainPlugin.js
+++ www/aimbrainPlugin.js
@@ -1,16 +1,16 @@
 module.exports = {
         enrol: function (userId,aimbrainAPIKey,aimbrainAPISecret, success, error) {
         var passParams = [];
-                passParams.put(userId);
-                passParams.put(aimbrainAPIKey);
-                passParams.put(aimbrainAPISecret);
+                passParams.push(userId);
+                passParams.push(aimbrainAPIKey);
+                passParams.push(aimbrainAPISecret);
             cordova.exec(success, error, 'aimbrainPlugin', "enrol", passParams);
         },
         authenticate: function (userId,aimbrainAPIKey,aimbrainAPISecret, success, error) {
         var passParams = [];
-                passParams.put(userId);
-                passParams.put(aimbrainAPIKey);
-                passParams.put(aimbrainAPISecret);
+                passParams.push(userId);
+                passParams.push(aimbrainAPIKey);
+                passParams.push(aimbrainAPISecret);
             cordova.exec(success, error, 'aimbrainPlugin', "authenticate", passParams);
         }
     };
